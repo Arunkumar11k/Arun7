6e4a51ef1c408a9978b4355d357ef7196ff47107
Arunkumar11k <k.arunkumar11595@gmail.com>
2022-01-04 16:45:13 +0530

    task1

---
+++ src/org/tcs/Sam.java
@@ -0,0 +1,15 @@
+package org.tcs;
+
+public class Sam {
+	private void samp() {
+
+		System.out.println("velu");
+		
+	}
+	public static void main(String[] args) {
+		Sam s  = new Sam();
+		s.samp();
+		
+	}
+
+}
